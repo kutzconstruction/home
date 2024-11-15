264efabe72cd6659f50f973c672f8501dfafa4da
kutzconstruction <kutzconstruction@gmail.com>
2024-11-15 12:37:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelector(".img1").addEventListener("mousedown", func01);
+document.querySelector(".img2").addEventListener("mousedown", func02);
+
+function func01() {
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelector(".img1").setAttribute("class", "img11");
+  document.querySelector(".img2").setAttribute("class", "img22");
+}
+
+function func02() {
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
+  document.querySelector(".img11").setAttribute("class", "img1");
+  document.querySelector(".img22").setAttribute("class", "img2");
+}
