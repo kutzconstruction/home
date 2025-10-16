7e6b729cc2cd65e9ab38d342e2aef3be67029f79
kutzconstruction <kutzconstruction@gmail.com>
2025-10-16 14:05:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function func01() {
   document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
   document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
   document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
+  document.querySelectorAll(".a2")[0].setAttribute("class", "a22");
   document.querySelector(".img1").setAttribute("class", "img11");
   document.querySelector(".img2").setAttribute("class", "img22");
 }
 
 function func02() {
+  document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
   document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
   document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
   document.querySelectorAll(".a22")[0].setAttribute("class", "a2");
